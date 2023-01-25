8da37b464c5d2e6b65d958f1fd4a0be3890fded1
Alvin-Mwangi-W <alvin.wacira.m@gmail.com>
2023-01-25 16:03:43 +0300

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'My Projects' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'My Services' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
+});
+
+module.exports = router;
